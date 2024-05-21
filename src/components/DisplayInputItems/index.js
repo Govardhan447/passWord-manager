fc2c0b993a596b37cd60739dd7bf6f643c90f659
Govardhan447 <163231039+Govardhan447@users.noreply.github.com>
2024-05-21 17:24:59 +0530

    Update index.js

--- src/components/DisplayInputItems/index.js
+++ src/components/DisplayInputItems/index.js
@@ -1,39 +1,56 @@
+import {Component} from 'react'
 import './index.css'
 
-const DisplayInputItems = props => {
-  const {passwordDetails, updatePasswordList, isPasswordShow} = props
-  const {id, username, website, password, classname} = passwordDetails
-
-  console.log(classname)
-  const classNaming = `initial-heading ${classname}`
-
-  const passWord = isPasswordShow ? password : '***********'
-
-  const onclickDelete = () => {
+class DisplayInputItems extends Component {
+  onclickDelete = () => {
+    const {updatePasswordList, passwordDetails} = this.props
+    const {id} = passwordDetails
     updatePasswordList(id)
-    console.log(id)
   }
 
-  return (
-    <li className="details-container">
-      <div className="intial-container">
-        <h1 className={classNaming}>{website[0].toUpperCase()}</h1>
-      </div>
-      <div className="password-Details-container">
-        <p className="paragraph">{website}</p>
-        <p className="paragraph">{username}</p>
-        <p className="paragraph">{passWord}</p>
-      </div>
-      <div className="delete-container" data-testid="delete">
-        <button className="btn-delete" type="button" onClick={onclickDelete}>
-          <img
-            className="delete-icon"
-            src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png"
-            alt="delete"
-          />
-        </button>
-      </div>
-    </li>
-  )
+  render() {
+    const {passwordDetails, isPasswordShow} = this.props
+    const {username, website, password, classname} = passwordDetails
+
+    console.log(classname)
+    const classNaming = `initial-heading ${classname}`
+
+    const passWord = isPasswordShow ? (
+      <p className="paragraph">{password}</p>
+    ) : (
+      <img
+        className="stars"
+        src="https://assets.ccbp.in/frontend/react-js/password-manager-stars-img.png"
+        alt="stars"
+      />
+    )
+
+    return (
+      <li className="details-container">
+        <div className="intial-container">
+          <h1 className={classNaming}>{website[0].toUpperCase()}</h1>
+        </div>
+        <div className="password-Details-container">
+          <p className="paragraph">{website}</p>
+          <p className="paragraph">{username}</p>
+          {passWord}
+        </div>
+        <div className="delete-container">
+          <button
+            className="btn-delete"
+            type="button"
+            onClick={this.onclickDelete}
+            testid="delete"
+          >
+            <img
+              className="delete-icon"
+              src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png"
+              alt="delete"
+            />
+          </button>
+        </div>
+      </li>
+    )
+  }
 }
 export default DisplayInputItems
